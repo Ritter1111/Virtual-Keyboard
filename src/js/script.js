9c81604d4e06ff96792fd279f1e35a775a6691a8
Ritter1111 <86166867+Ritter1111@users.noreply.github.com>
2023-04-28 19:02:48 +0300

    fix: change design

--- src/js/script.js
+++ src/js/script.js
@@ -108,56 +108,55 @@ document.addEventListener('keydown', (e) => {
   const textArea = document.querySelector('.textarea');
   const kursorEnd = textArea.selectionEnd;
   const kursorStart = textArea.selectionStart;
-  const keyCode = e.code;
   const keyboard = currentLanguage === 'keyboardEng' ? keyboardEng : keyboardRu;
   const keyboardObj = keyboard.find((key) => key.code === e.code);
   if (keyboardObj === undefined) {
     return;
   }
   const keyInput = isShiftPressed ? keyboardObj.key2 : keyboardObj.key;
-  if ((keyCode === 'AltLeft' && e.ctrlKey)
-  || (keyCode === 'AltRight' && e.ctrlKey)
-  || (keyCode === 'ControlLeft' && e.altKey)
-  || (keyCode === 'ControlRight' && e.altKey)) {
+  if ((e.code === 'AltLeft' && e.ctrlKey)
+  || (e.code === 'AltRight' && e.ctrlKey)
+  || (e.code === 'ControlLeft' && e.altKey)
+  || (e.code === 'ControlRight' && e.altKey)) {
     switchKeyboard();
   }
-  if (keyCode === 'Enter') {
+  if (e.code === 'Enter') {
     insertCursor('\n');
-  } else if (keyCode === 'ArrowLeft') {
+  } else if (e.code === 'ArrowLeft') {
     textArea.value += '\u25C4';
-  } else if (keyCode === 'ArrowRight') {
+  } else if (e.code === 'ArrowRight') {
     textArea.value += '\u25BA';
-  } else if (keyCode === 'ArrowUp') {
+  } else if (e.code === 'ArrowUp') {
     textArea.value += '\u25B2';
-  } else if (keyCode === 'ArrowDown') {
+  } else if (e.code === 'ArrowDown') {
     textArea.value += '\u25BC';
-  } else if (keyCode === 'Backspace') {
+  } else if (e.code === 'Backspace') {
     if (kursorStart > 0) {
       textArea.value = textArea.value.slice(0, kursorStart - 1) + textArea.value.slice(kursorStart);
       textArea.setSelectionRange(kursorStart - 1, kursorStart - 1);
     }
-  } else if (keyCode === 'Delete') {
+  } else if (e.code === 'Delete') {
     textArea.value = textArea.value.slice(0, kursorEnd) + textArea.value.slice(kursorEnd + 1);
     textArea.selectionEnd = kursorEnd;
-  } else if (keyCode === 'Tab') {
+  } else if (e.code === 'Tab') {
     insertCursor('    ');
-  } else if (keyCode === 'Space') {
+  } else if (e.code === 'Space') {
     insertCursor(' ');
-  } else if (keyCode === 'ShiftRight' || keyCode === 'ShiftLeft') {
+  } else if (e.code === 'ShiftRight' || e.code === 'ShiftLeft') {
     isShiftPressed = true;
     renderNewKeyboard();
-  } else if (keyCode === 'CapsLock') {
+  } else if (e.code === 'CapsLock') {
     isCaps = !isCaps;
     renderNewKeyboard();
-  } else if (keyCode !== 'CapsLock'
-  && keyCode !== 'ShiftRight'
-  && keyCode !== 'ShiftLeft'
-  && keyCode !== 'ControlLeft'
-  && keyCode !== 'ControlLeft'
-  && keyCode !== 'ControlRight'
-  && keyCode !== 'AltRight'
-  && keyCode !== 'AltLeft'
-  && keyCode !== 'MetaLeft') {
+  } else if (e.code !== 'CapsLock'
+  && e.code !== 'ShiftRight'
+  && e.code !== 'ShiftLeft'
+  && e.code !== 'ControlLeft'
+  && e.code !== 'ControlLeft'
+  && e.code !== 'ControlRight'
+  && e.code !== 'AltRight'
+  && e.code !== 'AltLeft'
+  && e.code !== 'MetaLeft') {
     if (isCaps) {
       insertCursor(keyInput.toUpperCase());
     } else {
@@ -165,11 +164,11 @@ document.addEventListener('keydown', (e) => {
     }
   }
   button.forEach((btn) => {
-    if (btn.dataset.code === keyCode) {
+    if (btn.dataset.code === e.code) {
       textArea.focus();
       btn.classList.add('active');
     }
-    if (keyCode === 'CapsLock') {
+    if (e.code === 'CapsLock') {
       if (!btn.classList.contains('del')
       && !btn.classList.contains('backspace')
       && !btn.classList.contains('shift')
@@ -184,13 +183,12 @@ document.addEventListener('keydown', (e) => {
 document.addEventListener('keyup', (e) => {
   e.preventDefault();
   const button = document.querySelectorAll('.key');
-  const keyCode = e.code;
-  if (keyCode === 'ShiftRight' || keyCode === 'ShiftLeft' || e.code === 'CapsLock') {
+  if (e.code === 'ShiftRight' || e.code === 'ShiftLeft' || e.code === 'CapsLock') {
     isShiftPressed = false;
     renderNewKeyboard();
   }
   button.forEach((btn) => {
-    if (btn.dataset.code === keyCode) {
+    if (btn.dataset.code === e.code) {
       btn.classList.remove('active');
     }
   });
